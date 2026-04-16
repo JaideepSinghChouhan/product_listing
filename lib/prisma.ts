6eed7581dd0fb2980d2a5b9b8f254aa5f1d75ee4
JaideepSinghChouhan <jd8769023@gmail.com>
2026-04-16 23:28:30 +0530

    DB overload by vercel fixed in prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,26 +1,14 @@
-// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const pool = new Pool({
-  connectionString: process.env.DIRECT_URL,
-});
-
-const adapter = new PrismaPg(pool);
-
-const prisma =
+export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter, // 🔥 REQUIRED
+    log: ["error"],
   });
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production")
   globalForPrisma.prisma = prisma;
-}
-
-export { prisma };
