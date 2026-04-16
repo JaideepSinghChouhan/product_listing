83bfb7786e3ad732dad01a9cce316703286c19e3
JaideepSinghChouhan <jd8769023@gmail.com>
2026-04-16 23:03:56 +0530

    Fixed empty components issue

---
+++ app/admin/components/videos/VideosSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+'use client';
+
+
+export default function VideosSection() {
+    return (
+        <div className="videos-section">
+            <h1>Videos</h1>
+            <p>Videos content goes here</p>
+        </div>
+    );
+}
