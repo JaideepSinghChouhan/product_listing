2ea4566da86237b25d884709fc81943e3dbe83ed
JaideepSinghChouhan <jd8769023@gmail.com>
2026-04-23 00:11:57 +0530

    Rearrranging Video section after ProductsCTAsection

--- app/page.tsx
+++ app/page.tsx
@@ -47,9 +47,9 @@ export default function HomePage() {
         <HeroSection />
         <CategoriesSection />
         <FeaturedProductsSection />
-        <VideoSection />
         <AboutSection />
         <ProductsCtaSection />
+        <VideoSection />
         <TestimonialsSection />
         <ContactSection />
       </main>
